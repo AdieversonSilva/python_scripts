d36bc0808b11b282721007356e07e0bda24d9162
AdieversonSilva <adieverson.pro@gmail.com>
2024-09-09 18:21:01 -0300

    Trabalhando com Datetime

---
+++ Scripts-Python/1-datetime.py
@@ -0,0 +1,13 @@
+# Manipulando Datas - importando date, datetime e time
+from datetime import date, datetime, time
+
+data = date(2024, 9, 9)
+print(data)
+print(date.today())
+
+data_hora = datetime(2024, 9, 9)
+print(data_hora)
+print(datetime.today())
+
+hora = time(16, 23, 0)
+print(hora)
---
+++ Scripts-Python/2-datetime.py
@@ -0,0 +1,19 @@
+# Manipulando Datas - Com timedelta
+from datetime import timedelta, datetime
+
+tipo_carro = 'm'
+tempo_pequeno = 30
+tempo_medio = 45
+tempo_grande = 60
+
+data_atual = datetime.now()
+
+if tipo_carro == 'p':
+    data_estimada = data_atual + timedelta(minutes=tempo_pequeno)
+    print(f'O carro chegou: {data_atual} e ficará pronto às {data_estimada}')
+elif tipo_carro == 'm':
+    data_estimada = data_atual + timedelta(minutes=tempo_medio)
+    print(f'O carro chegou: {data_atual} e ficará pronto às {data_estimada}')
+else:
+    data_estimada = data_atual + timedelta(minutes=tempo_grande)
+    print(f'O carro chegou: {data_atual} e ficará pronto às {data_estimada}')
